1332809db39b4ba095bb3cfc185b967536771727
MIRETU JALETA <101978426+mijaleta@users.noreply.github.com>
2023-07-26 17:24:36 +0300

    Update contacts-ui.js

--- src/components/core-ui/contacts/contacts-ui.js
+++ src/components/core-ui/contacts/contacts-ui.js
@@ -43,7 +43,7 @@ const ContactUI = ({
     
     // Send a post request with input values
     try {
-      const response = await axios.post('/api/contacts', { name, email, message });
+      const response = await axios.post('process.env.REACT_APP_SERVER_URL/api/contacts', { name, email, message });
       const data = response.data;
       
       // Set success state and snackbar message
