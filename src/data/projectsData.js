9b65e77caa8a021fb6f2f67b9ec746c9403fbc6a
mijaletat <miratuujaallataa@gmail.com>
2023-10-20 18:25:46 +0300

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -29,6 +29,58 @@ export const projectsData = [
         demo: '#',
         image: eight
     }
+    ,    {
+        id: 4,
+        projectName: 'online bus ticket booking',
+        projectDesc: 'Booking the ticket for bus .',
+        tags: [ 'SAAS', 'React Bootstrap'],
+        code: '',
+        demo: 'https://miretu-jaleta.netlify.app/',
+        image: six
+    },
+    {
+        id: 5,
+        projectName: 'Photo Studio',
+        projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
+        tags: ['Bootstrap', 'Express', 'JavaScript'],
+        code: '#',
+        demo: 'https://duna-photo-studio.netlify.app/',
+        image: seven
+    },
+    {
+        id: 6,
+        projectName: 'Portfolio Site',
+        projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
+        code: '#',
+        demo: '#',
+        image: eight
+    }
+    ,    {
+        id: 7,
+        projectName: 'online bus ticket booking',
+        projectDesc: 'Booking the ticket for bus .',
+        tags: [ 'SAAS', 'React Bootstrap'],
+        code: '',
+        demo: 'https://miretu-jaleta.netlify.app/',
+        image: six
+    },
+    {
+        id: 8,
+        projectName: 'Photo Studio',
+        projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
+        tags: ['Bootstrap', 'Express', 'JavaScript'],
+        code: '#',
+        demo: 'https://duna-photo-studio.netlify.app/',
+        image: seven
+    },
+    {
+        id: 9,
+        projectName: 'Portfolio Site',
+        projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
+        code: '#',
+        demo: '#',
+        image: eight
+    }
 ]
 
 
