babfa963a0288c47338420ebdfde35a8b01f007f
mijaletat <miratuujaallataa@gmail.com>
2023-10-20 21:48:29 +0300

    photo added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,7 @@
 import eight from '../assets/svg/projects/brr22.png'
 import seven from '../assets/svg/projects/brr33.png'
 import six from '../assets/svg/projects/brr11.png'
+import smsgateway from '../assets/svg/projects/sms-gateway.png'
 // import duna from '../assets/svg/projects/duna svg.svg'
 export const projectsData = [
     {
@@ -11,9 +12,19 @@ export const projectsData = [
         code: '',
         demo: 'https://miretu-jaleta.netlify.app/',
         image: six
-    },
-    {
+    }
+    ,    {
         id: 2,
+        projectName: 'sms gateway',
+        projectDesc: 'It is all about sms gateway .',
+        tags: [ 'SAAS', 'Bootstrap','html','css'],
+        code: '',
+        demo: 'https://message-link.bomerc.com/',
+        image: smsgateway
+    },
+    
+    {
+        id: 4,
         projectName: 'Photo Studio',
         projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
         tags: ['Bootstrap', 'Express', 'JavaScript'],
@@ -25,62 +36,12 @@ export const projectsData = [
         id: 3,
         projectName: 'Portfolio Site',
         projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
+        tags:['Bootstrap', 'Express', 'JavaScript'],
         code: '#',
         demo: '#',
         image: eight
     }
-    ,    {
-        id: 4,
-        projectName: 'online bus ticket booking',
-        projectDesc: 'Booking the ticket for bus .',
-        tags: [ 'SAAS', 'React Bootstrap'],
-        code: '',
-        demo: 'https://miretu-jaleta.netlify.app/',
-        image: six
-    },
-    {
-        id: 5,
-        projectName: 'Photo Studio',
-        projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
-        tags: ['Bootstrap', 'Express', 'JavaScript'],
-        code: '#',
-        demo: 'https://duna-photo-studio.netlify.app/',
-        image: seven
-    },
-    {
-        id: 6,
-        projectName: 'Portfolio Site',
-        projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
-        code: '#',
-        demo: '#',
-        image: eight
-    }
-    ,    {
-        id: 7,
-        projectName: 'online bus ticket booking',
-        projectDesc: 'Booking the ticket for bus .',
-        tags: [ 'SAAS', 'React Bootstrap'],
-        code: '',
-        demo: 'https://miretu-jaleta.netlify.app/',
-        image: six
-    },
-    {
-        id: 8,
-        projectName: 'Photo Studio',
-        projectDesc: 'This the project i have done when i am apparent at debo engineering corporate, It also more about photo studio',
-        tags: ['Bootstrap', 'Express', 'JavaScript'],
-        code: '#',
-        demo: 'https://duna-photo-studio.netlify.app/',
-        image: seven
-    },
-    {
-        id: 9,
-        projectName: 'Portfolio Site',
-        projectDesc: 'This project is the project i have done when i was intern at debo engineering corporate.',
-        code: '#',
-        demo: '#',
-        image: eight
-    }
+
 ]
 
 
