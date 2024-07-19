66e64d13df08409dde263f98e31cddb9909caa2f
MIRETU JALETA <101978426+mijaleta@users.noreply.github.com>
2024-07-19 10:50:13 +0300

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,8 +5,8 @@ export const headerData = {
     desciption: "Hello! I am Miretu Jaleta, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     // image: 'https://i.ibb.co/7bnmm3h/m12.png',
     
-    image: 'https://ibb.co/7jQxHLT',
-    image2: 'https://i.ibb.co/Xb1p0SR/mpp.jpg',
+    image: 'https://i.ibb.co/Xb1p0SR/mpp.jpg',
+    image2: 'https://ibb.co/7jQxHLT',
 
     imagebw: 'https://i.ibb.co/7bnmm3h/m12.png',
     resumePdf: 'https://drive.google.com/file/d/1VpIqcPse4m_EKC0HNli5oUdh_UrtWOUK/view?usp=sharing'
