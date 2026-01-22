ce8956b090d241e71940f4b6ace9cf36ce9ce502
Miretu Jaleta <miratuujaallataa@gmail.com>
2026-01-22 11:33:54 +0300

    updated

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'miratuujaallataa@gmail.com',
     phone: '+251 73284058',
-    address: 'Jimma Ethiopia',
+    address: 'Addis Ababa, Ethiopia',
     github: 'https://github.com/mijaleta',
     facebook: 'https://www.facebook.com/miretu.jaleta',
     linkedIn: 'https://www.linkedin.com/in/miretu-jaleta-995b22231/',
